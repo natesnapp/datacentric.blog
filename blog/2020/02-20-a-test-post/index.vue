a6e6a47ada5bb6d4d6483ebf30bff69e3c8f7a2d
Nate Snapp <nathansn@adobe.com>
2019-03-28 10:32:47 -0700

    Data Centric updates
    Also added test post

---
+++ blog/2020/02-20-a-test-post/index.vue
@@ -0,0 +1,36 @@
+<script>
+  export default {
+    title: 'A Test Post',
+    tags: ['security', 'deployment', 'cloud'],
+    tldr: [
+      'Cloud deployments have inherent insecurities',
+      'Most common insecurities are cloud logging, access permissions'
+    ]
+  }
+</script>
+
+<template lang="md">
+
+  ## Preface
+
+  This is a test post
+
+  Just some test text that will be enhanced later
+</template>
+
+<style lang="less" scoped>
+  .word-break-normal {
+    word-break: normal;
+  }
+  .update {
+    font-style: italic;
+    font-family: Georgia, serif;
+    margin-bottom: 2em;
+    p {
+      margin-bottom: 5px;
+    }
+    blockquote {
+      margin: 12px 0;
+    }
+  }
+</style>
